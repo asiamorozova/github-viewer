030546ebf6882b79fe6b4bcc1ec34410e9f11a01
Anastasia Morozova <morozova.asia@gmail.com>
2020-04-28 18:48:49 -0700

    user input

---
+++ src/components/User/UserInput.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserInput = ({ username, onUserChange, onUserSubmit }) => (
+  <>
+    <input type="text" name="username" value={username} onChange={onUserChange} />
+    <input type="button" name="search" value="Search" onClick={onUserSubmit} />
+  </>
+);
+UserInput.propTypes = {
+  username: PropTypes.string.isRequired,
+  onUserChange: PropTypes.func.isRequired,
+  onUserSubmit: PropTypes.func.isRequired
+};
+export default UserInput;
